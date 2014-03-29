4d93a8262a597c72855a7d70adc0553cd70e927c
Dan Hassin <danhassin@mac.com>
2014-03-29 15:57:36 -0400

    Parse if statement

--- parse.js
+++ parse.js
@@ -1,15 +1,29 @@
 var esprima = require("esprima");
 
-var parseFunctions = {'Program':parseProgram, 'End':parseEnd, 'VariableDeclaration':parseVD};
+var parseFunctions = 
+{'Program':parseProgram, 
+'End':parseEnd, 
+'VariableDeclaration':parseVD, 
+'IfStatement':parseIf, 
+'BlockStatement':parseBS,
+'BinaryExpression':parseBE};
+
 var hash = {};
 
-var BODY = 0;
-var FOLLOW = 1;
-var END_NODE = {type:"End"}
+var BODY = "B";
+var FOLLOW = "F";
+var IF_CONS = "IF_C";
+var IF_ALT = "IF_A";
+var IF_TEST = "IF_T";
+
+var END_NODE = {type:"End"};
+
 
 var DEPTH = 2;
 
-parseFile('var answer = 42; var a = 30; var b = 20; var c = 1;');
+//parseFile('var answer = 42; var a = 30; var b = 20; var c = 1;');
+parseFile("if (a=='5') { var a = 10; } else var b = 3;");
+
 console.log(JSON.stringify(hash, null, 2));
 
 function traverse(path)
@@ -26,23 +40,45 @@ function traverse(path)
 	return working;
 }
 
+function addCount(node, path)
+{
+	if (path && path.length >= DEPTH*2)
+	{
+		var probs = traverse(path);
+		probs[node.type] = (probs[node.type] || 0) + 1;
+	}
+}
+
 function parseEnd(end, path)
 {
-	console.log("parsing end ");
-
-	var probs = traverse(path);
-	probs[end.type] = (probs[end.type] || 0) + 1;
 }
 
 function parseVD(vd, path)
 {
-	console.log("parsing VD path=",path);
+}
 
-	if (path.length >= DEPTH*2)
+function parseBE(node, path)
+{
+}
+
+function parseIf(node, path)
+{
+	parseNode(node.test, path.concat([node.type, IF_TEST]));
+	parseNode(node.consequent, path.concat([node.type, IF_CONS]));
+	parseNode(node.alternate || END_NODE, path.concat([node.type, IF_ALT]));
+}
+
+function parseBS(node, path)
+{
+	path = path.concat([node.type, BODY]);
+	var statements = node.body;
+	for (var i = 0; i < statements.length; i++)
 	{
-		var probs = traverse(path);
-		probs[vd.type] = (probs[vd.type] || 0) + 1;
+		var statement = statements[i];
+		parseNode(statement, path);
+		path = path.concat([statement.type, FOLLOW]);
 	}
+	parseNode(END_NODE, path);
 }
 
 function parseProgram(program)
@@ -54,7 +90,7 @@ function parseProgram(program)
 	var path = [program.type, BODY];
 	for (var i = 0; i < statements.length; i++)
 	{
-		var statement = program.body[i];
+		var statement = statements[i];
 
 		if (i == 0)
 		{
@@ -71,8 +107,12 @@ function parseProgram(program)
 
 function parseNode(node, path)
 {
-	//var level traverse(path)
-	parseFunctions[node.type](node, path);
+	addCount(node, path);
+	console.log("parsing ", node.type, " path=",path);
+	if (parseFunctions[node.type])
+	{
+		parseFunctions[node.type](node, path);
+	}
 }
 
 function parseFile(text)
