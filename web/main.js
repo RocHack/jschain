0d3027098dc9e4d9f6c9784e1494d1e2fde37f10
Charles Lehner <celehner1@gmail.com>
2014-04-20 12:26:11 -0400

    Add getPath

--- web/main.js
+++ web/main.js
@@ -31,6 +31,48 @@ window.registerNodeWithCurrentPath = function (node) {
 	astDomMap[node] = currentPosition;
 }
 
+var hasOwnProp = Function.prototype.call.bind(Object.prototype.hasOwnProperty);
+
+function objectsEqual(a, b) {
+	return JSON.stringify(a) == JSON.stringify(b);
+}
+
+var FOLLOW = "F";
+// looking for node in syntax
+function getPath(syntax, node, depth, path) {
+	console.log(syntax, node, depth, path);
+	if (!path) {
+		path = [];
+	}
+	if (!syntax || typeof syntax != "object") {
+		return;
+	}
+	if (objectsEqual(syntax, node)) {
+		return path;
+	}
+	if (syntax.type) {
+		path = path.concat(syntax.type);
+		if (depth) {
+			path = path.slice(-depth*2);
+		}
+	}
+	if (syntax instanceof Array) {
+		for (var i = 0; i < syntax.length; i++) {
+			var item = syntax[i];
+			var p = getPath(item, node, depth, path);
+			if (p) return p;
+			path.push(item.type, FOLLOW);
+		}
+		return;
+	}
+	for (var key in syntax) {
+		if (key != "type" && hasOwnProp(syntax, key)) {
+			var p = getPath(syntax[key], node, depth, path.concat(key));
+			if (p) return p;
+		}
+	}
+}
+
 function setCurrentPath(path) {
 	currentPosition = path;
 }
